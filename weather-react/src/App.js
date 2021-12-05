78a6dee2f83324ffb7fe668e2e72c7ba83c07c4b
jwiergowska <juliajestsuper@gmail.com>
2021-12-05 14:37:32 +0100

    that's it

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-      
-          Hello World
-        </h1>
-
-        <Weather />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
